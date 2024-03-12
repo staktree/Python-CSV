014774406108ca9774b6f724deff700b7340dead
Tak <tak@tak.com>
2024-03-13 01:07:01 +0900

    encoding changed

--- PyExem/firstPy.py
+++ PyExem/firstPy.py
@@ -1,6 +1,6 @@
 import os
 import csv
-#from googletrans import Translator
+from googletrans import Translator
 
 ### 모든 파일 가져오기
 def get_folder_names(file_path):
@@ -28,7 +28,9 @@ def print_list(lst):
   print("----------------\n")
 
 ### 모든 파일 정보 수정하기
-lang_list = ["KOR", "ENG", "JPN", "CHN"]
+
+# TODO : 타겟 언어 설정
+lang_list = ["ko", "en", "ja", "zh-CN", "zh-TW", "fr", "vi", "th"]
 
 def file_update_process(file):
   text_list = []
@@ -42,7 +44,8 @@ def file_update_process(file):
 
   print("Processing " + file)
 
-  with open(file, 'r', encoding='cp949') as f:
+  # TODO : encoding 형식 설정
+  with open(file, 'r', encoding='utf-8-sig') as f:
     reader = csv.reader(f)
     for row in reader:
       text_list.append(row)
@@ -50,28 +53,33 @@ def file_update_process(file):
     for row in text_list:
       exist_lang.append(row[0])
 
-    if 'ENG' in exist_lang:
-      translating_data = [row for row in text_list if row[0] == 'ENG']
+    # 원본 언어 설정
+    if 'en' in exist_lang:
+      translating_data = [row for row in text_list if row[0] == 'en']
     else:
-      translating_data = [row for row in text_list if row[0] == 'KOR']
+      translating_data = [row for row in text_list if row[0] == 'ko']
+
+    print(translating_data)
 
     adding_lang = [lang for lang in lang_list if lang not in exist_lang]
 
     for lang in adding_lang:
       translated_data = []
       translated_data.append(lang)
+      if len(translating_data) == 0:
+        continue
       for text in translating_data[0][1:]:
         translated_data.extend([translate_to_otherLang(text, lang)])
       translated_list.append(translated_data)
 
+    # TODO : encoding 형식 설정
     for translated_data in translated_list:
-       with open(file, 'a', newline='', encoding='cp949') as f:
+       with open(file, 'a', newline='', encoding='utf-8-sig') as f:
          writer = csv.writer(f)
          writer.writerow(translated_data)
 
 def translate_to_otherLang(text, target_lang):
-  #return Translator().translate(text, dest=target_lang).text
-  return text
+  return Translator().translate(text, dest=target_lang).text
 
 
 def translate_to_allLang(file_list):
@@ -80,8 +88,8 @@ def translate_to_allLang(file_list):
 
 
 # 실행
-print("Hello Coding")
-file_path = 'C:/Users/86168/Desktop/DataTest' #'C:\Users\86168\Desktop\DataTest'
+print("Translate")
+file_path = "your dir"
 file_list = list = get_folder_names(file_path)
 print_list(file_list)
 translate_to_allLang(file_list)
