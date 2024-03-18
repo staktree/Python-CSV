ab257a5dbf93313041efe22fb249868cc3bfb2c5
unknown <jht0201@lgcns.com>
2024-03-18 10:17:52 +0900

    secondPy.py Added.

---
+++ PyExem/secondPy.py
@@ -0,0 +1,116 @@
+import os
+import csv
+from googletrans import Translator
+
+### 모든 파일 가져오기
+def get_folder_names(file_path):
+  file_list = []
+  if not check_folder_exists(file_path):
+    return
+
+  for root, dirs, files in os.walk(file_path):
+    for file_name in files:
+      file_list.append(os.path.join(root, file_name))
+  return file_list
+
+def check_folder_exists(folder_path):
+  if os.path.exists(folder_path):
+    print("Folder exists.")
+    return True
+  else:
+    print("Folder does not exist.")
+    return False
+
+def print_list(lst):
+  if not lst: return
+  for item in lst:
+    print(item)
+  print("----------------\n")
+
+### 모든 파일 정보 수정하기
+
+# TODO : 타겟 언어 설정
+lang_list = ["ko", "en", "ja", "zh-CN", "zh-TW", "fr", "vi", "th"]
+
+def file_update_process(file):
+  text_list = []
+  adding_lang = []
+  exist_lang = []
+  translated_list = []
+  translating_data = []
+
+  if not file.endswith('.csv'):
+    return
+
+  print("Processing " + file)
+
+  # TODO : encoding 형식 설정
+  with open(file, 'r', encoding='utf-8-sig') as f:
+    reader = csv.reader(f)
+    text_list = list(reader)
+
+  text_list = list(map(list, zip(*text_list)))
+
+  for row in text_list:
+    exist_lang.append(row[0])
+
+  print(exist_lang)
+
+  # 원본 언어 설정
+  if 'en' in exist_lang:
+      translating_data = [row for row in text_list if row[0] == 'en']
+  else:
+      translating_data = [row for row in text_list if row[0] == 'ko']
+
+  print(translating_data)
+
+  #adding_lang = [lang for lang in lang_list if lang not in exist_lang]
+  adding_lang = lang_list;
+
+  for lang in adding_lang:
+    translated_data = []
+    translated_data.append(lang)
+    if len(translating_data) == 0:
+      continue
+    for text in translating_data[0][1:]:
+      translated_data.extend([translate_to_otherLang(text, lang)])
+    translated_list.append(translated_data)
+
+
+  translated_list = list(map(list, zip(*translated_list)))
+  print(translated_list)
+
+  def delete_file_data(file):
+    with open(file, 'w', encoding='utf-8-sig') as f:
+      f.truncate(0)
+
+  # 파일 초기화
+  print("Delete File Data")
+  file_list = get_folder_names(file_path)
+  print_list(file_list)
+  for file in file_list:
+    delete_file_data(file)
+
+  # TODO : encoding 형식 설정
+  # 파일 쓰기
+  for translated_data in translated_list:
+    with open(file, 'a', newline='', encoding='utf-8-sig') as f:
+      writer = csv.writer(f)
+      writer.writerow(translated_data)
+
+def translate_to_otherLang(text, target_lang):
+  if(target_lang == "ko"): return text
+  return text + " " + target_lang # 지워야함
+  return Translator().translate(text, dest=target_lang).text
+
+
+def translate_to_allLang(file_list):
+   for file in file_list:
+     file_update_process(file)
+
+# 실행
+print("Translate")
+file_path = "Your File Path"
+file_list = get_folder_names(file_path)
+print_list(file_list)
+translate_to_allLang(file_list)
--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-1. filepath 설정하기 
-#실행 
-file_path = "C:/Users/TAK/Downloads/CSVData/CSVData/Language/" >> 원하는 파일이 있는 상위폴더로 지정 시 모든 하위 파일에 대해 작업을 수행합니다.
+# 공통
+
+1. filepath 설정하기 #실행
+   file_path = "C:/Users/TAK/Downloads/CSVData/CSVData/Language/" >> 원하는 파일이 있는 상위폴더로 지정 시 모든 하위 파일에 대해 작업을 수행합니다.
 
 2. csv 파일 설정
-다른 이름 저장 > csv(etf-8)으로 속성 지정하여 저장.
+   다른 이름 저장 > csv(etf-8)으로 속성 지정하여 저장.
 
-3. from googletrans import Translator 오류 
-pip를 통해 googletrans install.
+3. from googletrans import Translator 오류
+   pip를 통해 googletrans install.
 
-4. 권한 오류 
-작업 예정인 파일이 오픈되어있지는 않은지 확인.
+4. 권한 오류
+   작업 예정인 파일이 오픈되어있지는 않은지 확인.
 
 5. googletrans 오류 시 버전 변경하여 재설치.
+
+# firstPy
+
+csv파일의 1행 데이터를 번역하여 새로운 행으로 추가하는 예제.
+
+# secondPy
+
+csv파일의 1열 데이터를 번역하여 새로운 열로 추가하는 예제.
+reader 객체는 데이터를 추가할 때 기본적으로 1행씩 추가하기 때문에, 데이터 삽입 전
+기존의 데이터를 모두 삭제한 후에 추가하였음.
